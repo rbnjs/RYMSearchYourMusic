5cbc6cc7d70fb9dafd0ca6a59bb553f44a5c4c82
RubenSerradas <rubenserradas@gmail.com>
2015-08-14 19:56:40 -0430

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# RYMSearchYourMusic
+
+Not Ready.
---
+++ RYMSeasrchYourMusic.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        RYMSearchYourMusic
+// @namespace   https://greasyfork.org/users/14256
+// @description Search in YouTube the music that you are viewing in RYM
+// @include     https://rateyourmusic.com/release/*
+// @include     http://rateyourmusic.com/release/*
+// @include     https://rateyourmusic.com/artist/*
+// @include     http://rateyourmusic.com/artist/*
+// @version     0.0.0
+// @grant       none
+// ==/UserScript==
