ebfbea417537774626e2e906083e2b912d175a44
RubenSerradas <rubenserradas@gmail.com>
2015-08-14 22:49:49 -0430

    Added Location Object.

--- RYMSeasrchYourMusic.user.js
+++ RYMSeasrchYourMusic.user.js
@@ -6,6 +6,18 @@
 // @include     http://rateyourmusic.com/release/*
 // @include     https://rateyourmusic.com/artist/*
 // @include     http://rateyourmusic.com/artist/*
-// @version     0.0.0
+// @version     0.1.0
 // @grant       none
 // ==/UserScript==
+
+Location = {
+  artistPage: function(){
+    var location = window.location.href;
+    var reg_ex = /\/artist/;
+    var info = reg_ex.exec(location);
+    if (info && (info.index == 25 || info.index == 24)){
+      return true;
+    }
+    return false;
+  }
+};
